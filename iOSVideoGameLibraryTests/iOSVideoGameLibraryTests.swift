0682723dd9a275184215cf154397cf33850782f5
ChristianLenTemp <christianlentemp@interapts-macbook-pro-92.local>
2018-04-12 14:42:20 -0400

    Initial Commit

---
+++ iOSVideoGameLibraryTests/iOSVideoGameLibraryTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSVideoGameLibraryTests.swift
+//  iOSVideoGameLibraryTests
+//
+//  Created by ChristianLenTemp on 4/12/18.
+//  Copyright © 2018 ChristianLenTemp. All rights reserved.
+//
+
+import XCTest
+@testable import iOSVideoGameLibrary
+
+class iOSVideoGameLibraryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
