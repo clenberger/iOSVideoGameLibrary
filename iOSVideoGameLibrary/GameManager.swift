c9d40ac893b097b2215f22e6d534811d66cbc734
ChristianLenTemp <christianlentemp@tigerpy-7.jefferson.ketsds.net>
2018-04-20 09:50:36 -0400

    Added segues and functionality improvements

---
+++ iOSVideoGameLibrary/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  iOSVideoGameLibrary
+//
+//  Created by ChristianLenTemp on 4/19/18.
+//  Copyright © 2018 ChristianLenTemp. All rights reserved.
+//
+
+import Foundation
---
+++ iOSVideoGameLibrary/VideoGameClass.swift
@@ -0,0 +1,9 @@
+//
+//  VideoGameClass.swift
+//  iOSVideoGameLibrary
+//
+//  Created by ChristianLenTemp on 4/18/18.
+//  Copyright © 2018 ChristianLenTemp. All rights reserved.
+//
+
+import Foundation
